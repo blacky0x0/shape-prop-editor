c25801d7fa9e4823d4d847ddb93535c66e96407b
blacky0x0 <blacky0x0@users.noreply.github.com>
2015-03-15 02:43:10 +0300

    Initial commit

---
+++ src/com/github/blacky0x0/editor/model/Shape.java
@@ -0,0 +1,23 @@
+package com.github.blacky0x0.editor.model;
+
+/**
+ * User: blacky
+ * Date: 15.03.15
+ */
+public abstract class Shape {
+    protected String name;
+    protected int x;
+    protected int y;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
