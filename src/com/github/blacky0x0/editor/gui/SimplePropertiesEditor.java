d1865b2344835fed44ca0de384c36bb99418c95c
blacky0x0 <blacky0x0@users.noreply.github.com>
2015-03-16 05:30:39 +0300

    Add a table without bindings

--- src/com/github/blacky0x0/editor/gui/SimplePropertiesEditor.java
+++ src/com/github/blacky0x0/editor/gui/SimplePropertiesEditor.java
@@ -1,13 +1,25 @@
 package com.github.blacky0x0.editor.gui;
 
+import java.awt.Point;
+import java.util.HashSet;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.SashForm;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
+
 import com.github.blacky0x0.editor.model.Rectangle;
 import com.github.blacky0x0.editor.model.Shape;
 import com.github.blacky0x0.editor.util.GuiUtil;
-import org.eclipse.swt.*;
-import org.eclipse.swt.widgets.*;
-
-import java.awt.Point;
-import java.util.HashSet;
 
 /**
  * User: blacky
@@ -39,6 +51,10 @@ public class SimplePropertiesEditor {
     private final String EXIT_TEXT = "E&xit";
     private final int EXIT_ACCELERATOR = SWT.MOD1 + 'X';
 
+    // Table
+    private Table table;
+    private SashForm sashForm;
+    private List list;
 
     public static void main (String [] args) {
         SimplePropertiesEditor editor = new SimplePropertiesEditor();
@@ -49,9 +65,67 @@ public class SimplePropertiesEditor {
 
         initMenu();
 
+        initTable();
+
         initData();
 
         finishInit();
+
+    }
+
+    private void initTable() {
+        
+        sashForm = new SashForm(shell, SWT.NONE);
+
+        table = new Table (sashForm, SWT.MULTI | SWT.BORDER | SWT.FULL_SELECTION);
+        table.setHeaderVisible (true);
+
+        list = new List(sashForm, SWT.BORDER);
+        sashForm.setWeights(new int[]{1, 1});
+
+        String[] titles = {"Name", "Type", "X", "Y"};
+
+//        TableColumn columnId = new TableColumn (table, SWT.NONE);
+//        columnId.setText ("id");
+//        columnId.setWidth(80);
+
+        TableColumn columnName = new TableColumn (table, SWT.NONE);
+        columnName.setText ("Name");
+        columnName.setWidth(80);
+
+        TableColumn columnType = new TableColumn (table, SWT.NONE);
+        columnType.setText ("Type");
+        columnType.setWidth(80);
+
+        TableColumn columnX = new TableColumn (table, SWT.NONE);
+        columnX.setText ("X");
+        columnX.setWidth(60);
+
+        TableColumn columnY = new TableColumn (table, SWT.NONE);
+        columnY.setText ("Y");
+        columnY.setWidth(60);
+
+//         for (int i=0; i<titles.length; i++) {
+//            TableColumn column = new TableColumn (table, SWT.NONE);
+//            column.setText (titles [i]);
+//         }
+
+        int count = 5;
+        for (int i = 0; i < count; i++) {
+            TableItem item = new TableItem (table, SWT.NONE);
+
+            item.setText (0, "name");
+            item.setText (1, "type");
+            item.setText (2, "x");
+            item.setText (3, "y");
+
+        }
+
+//        new TableItem (table, SWT.NONE).setData(new Rectangle(1, 2, "r", 10, 20));
+
+        for (int i = 0; i < titles.length; i++) {
+            table.getColumn(i).pack();
+        }
 
     }
 
@@ -86,6 +160,7 @@ public class SimplePropertiesEditor {
     }
 
     public void initMenu() {
+        shell.setLayout(new FillLayout(SWT.HORIZONTAL));
         shell.setMenuBar(bar);
 
         compositeFileItem = new MenuItem (bar, SWT.CASCADE);
@@ -117,6 +192,8 @@ public class SimplePropertiesEditor {
         });
 
         compositeFileItem.setMenu(compositeFileMenu);
+        
+        new MenuItem(compositeFileMenu, SWT.SEPARATOR);
 
         exit = new MenuItem (compositeFileMenu, SWT.PUSH);
 
